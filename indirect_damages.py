964bb69d090b1b8b8770b16ce4bf642beb814075
Daniel Peregrina Gonzalez <99656768+edPeregrina@users.noreply.github.com>
2024-04-12 17:08:10 +0200

    Added visualisations and cropping to flow model

--- indirect_damages.py
+++ indirect_damages.py
@@ -16,68 +16,41 @@ from networkx. The flow is then visualized using networkx.
 
 The next steps are to add costs to the edges for 
 economic appraisal.
-Demand data: EUSTAT 
-
-
+Demand data: EUSTAT rail_tf_ns20_de
 """	
-import pickle
-import numpy as np
-# from shapely.validation import make_valid # only needed to make invalid geometries valid
-from ci_adapt_utilities import *
-from ci_adapt_classes import *
-
-# configure handler with ini file. ini file can be created with config.py
-H=Handler(config_file='config_ci_adapt.ini')
-# read vulnerability and maximum damage data
-H.read_vul_maxdam()
-
-# read hazard data
-H.read_hazard_data()
-
-# read exposure data
-H.read_asset_data()
-
+#ADDITIONAL DEPENDENCIES
 # #!pip install triangle
 # #!pip install rasterio
 # #!pip install geopy
 # #!pip install osmnx
+
+#IMPORTS
 import sys
-import csv
-# import os
 sys.path.append(r'C:\repos\snkit\src')
 sys.path.append(r'C:\repos\ra2ce')
 sys.path.append(r'C:\repos\ra2ce_multi_network')
-# from ra2ce_multi_network.deeper_extraction import filter_on_other_tags
+from ci_adapt_utilities import *
+from ci_adapt_classes import *
 from ra2ce_multi_network.simplify_rail import *
-
-#Source: Asgarpour/snkit
-# Create a railway networks with possible terminal nodes. This returns a complex network, as includes the rail tracks with the highest level of detail.
-aggregation_range = 0.08 # in km
-complex_rail_network = get_rail_network_with_terminals(network_gdf=H.assets.assets, aggregation_range=aggregation_range)
-
-
-# making merged rail network #before this: create snkit network
 from ra2ce_multi_network.simplify_rail import _merge_edges, _network_to_nx
 import networkx as nx
-merged_rail_network = _merge_edges(network=complex_rail_network, excluded_edge_types=['bridge', 'tunnel']) #Must add network= to pass excluded_edge_types as a keyword argument
+import pandas as pd
+from shapely import wkt, box
 
-# Number of nodes and edges redduced
-
-print(f"Difference in node counts: {complex_rail_network.nodes.shape[0] - merged_rail_network.nodes.shape[0]}")
-print(f"Difference in node counts %: {round(100*(complex_rail_network.nodes.shape[0] - merged_rail_network.nodes.shape[0])/complex_rail_network.nodes.shape[0], 0)}")
-
-print(f"Difference in edge counts: {complex_rail_network.edges.shape[0] - merged_rail_network.edges.shape[0]}")
-print(f"Difference in edge counts %: {round(100*(complex_rail_network.edges.shape[0] - merged_rail_network.edges.shape[0])/complex_rail_network.edges.shape[0], 0)}")
-
-
-merged_rail_graph = _network_to_nx(merged_rail_network)
-
-G=nx.MultiDiGraph(merged_rail_graph)
-
+#FUNCTION DEFINITIONS
 #TODO: Add cost for economic appraisal, add mode factor to edge weights for multi-modality
+def get_bbox(graph):
+    min_x = min([attr['pos'][0] for _, attr in graph.nodes(data=True)])
+    max_x = max([attr['pos'][0] for _, attr in graph.nodes(data=True)])
+    min_y = min([attr['pos'][1] for _, attr in graph.nodes(data=True)])
+    max_y = max([attr['pos'][1] for _, attr in graph.nodes(data=True)])
+    return min_x, max_x, min_y, max_y
+
 def set_edge_weights(assets, graph):
     asset_lengths = {str(asset['osm_id']): asset['geometry'].length for asset_id, asset in assets.iterrows()}
     for u, v, attr in graph.edges(data=True):
+        if 'source_sink' in str(u) or 'source_sink' in str(v):
+            continue
         attr['weight'] = int(0)
         attr['length'] = 0
         if 'osm_id' not in attr:
@@ -131,101 +104,11 @@ def equalize_demand_supply(graph):
         if u == 'source_sink':
             continue
         if not graph.has_edge(u, 'source_sink', 0):
-            graph.add_edge(u,'source_sink',0)
-        if not graph.has_edge('source_sink', u, 0):
-            graph.add_edge('source_sink',u,0)
+            graph.add_edge(u,'source_sink', 0, weight=int(1e8))
+        if not graph.has_edge('source_sink', u):
+            graph.add_edge('source_sink', u, 0, weight=int(1e8))
 
     return graph
-
-
-od_data_source = r'C:\Data\interim\od_data_2.csv'
-import pandas as pd
-from shapely import wkt
-
-od_data = pd.read_csv(od_data_source)
-# Drop rows where 'geometry' is NaN
-od_data = od_data.dropna(subset=['geometry'])
-
-od_data['geometry'] = od_data['geometry'].apply(wkt.loads)
-
-
-
-for _, attr in G.nodes(data=True):
-    attr['demand'] = int(0)
-    attr['pos'] = (attr['geometry'].x, attr['geometry'].y)
-
-G = set_node_demand(od_data, G)
-G = equalize_demand_supply(G)
-
-
-
-G = set_edge_capacities(G)
-G = set_edge_weights(H.assets.assets, G)
-
-
-
-
-
-
-#Source: SA
-max_weight_graph = max(attr['weight'] for _, _, attr in G.edges(data=True))
-print('Max weight: '+str(max_weight_graph))
-max_capacity_graph = max(attr['capacity'] for _, _, attr in G.edges(data=True))
-print('Max capacity: '+str(max_capacity_graph))
-
-# create a virtual node with dummy nodes
-G_v=G.copy()
-# convert to int
-for u, v, key, attr in G.edges(keys=True, data=True):
-    G_v.add_edge((str(u) + '_d'), (str(v) + '_d'), **attr)
-
-for u in G.nodes:
-    G_v.add_edge(u,(str(u) + '_d'),capacity=max_capacity_graph*100,weight=int(round(max_weight_graph*1e3,0)))
-    G_v.add_edge((str(u) + '_d'),u,capacity=max_capacity_graph*100,weight=0)
-
-
-mcf=nx.min_cost_flow(G_v)
-print('Minimum cost flow: '+str(mcf))
-
-flow_dict = mcf
-
-
-import re
-extract_numeric_part = lambda s: int(re.search(r'\d+', s).group()) if re.search(r'\d+', s) else None
-
-for u,attr in G_v.nodes(data=True):
-    try:
-        if isinstance(u, int): 
-            pass
-        elif isinstance(u, str): 
-            attr['pos']=(G_v.nodes[extract_numeric_part(u)]['pos'][0]+0.0005,G_v.nodes[extract_numeric_part(u)]['pos'][-1]+0.0005)
-    except:
-        print(f'Error in setting node {u} position')    
-pos=nx.get_node_attributes(G_v, 'pos')
-
-
-
-    
-
-# draw_edges=nx.draw_networkx_edges(G_v,pos)
-# nx.draw(G_v, pos, 
-#         with_labels=False,
-#         node_size=0,
-#         node_color='grey')
-
-# def set_edge_weights(assets, graph):
-#     for asset_id, asset in assets.iterrows():
-#         asset_geometry = asset['geometry']
-#         asset_length = asset_geometry.length
-#         asset_osm_id = str(asset_id)
-#         for u, v, key, attr in graph.edges(keys=True, data=True):
-#             osm_ids = attr['osm_id'].split(';')
-#             if asset_osm_id in osm_ids:
-#                 attr['weight'] += asset_length
-
-
-    
-
 
 def inspect_graph(graph):
     edge_capacities_types = []
@@ -244,7 +127,143 @@ def inspect_graph(graph):
 
     return edge_capacities_types, edge_weights_types, node_demands_types
 
+#MAIN
+# configure handler with ini file. ini file can be created with config.py
+H=Handler(config_file='config_ci_adapt.ini')
+
+# read exposure data
+H.read_asset_data()
+
+#Source: Asgarpour/snkit
+# Create a railway networks with possible terminal nodes. This returns a complex network, as includes the rail tracks with the highest level of detail.
+aggregation_range = 0.08 # in km
+complex_rail_network = get_rail_network_with_terminals(network_gdf=H.assets.assets, aggregation_range=aggregation_range)
+merged_rail_network = _merge_edges(network=complex_rail_network, excluded_edge_types=['bridge', 'tunnel']) #Must add network= to pass excluded_edge_types as a keyword argument
+
+# Number of nodes and edges reduced
+print(f"Difference in node counts: {complex_rail_network.nodes.shape[0] - merged_rail_network.nodes.shape[0]}")
+print(f"Difference in node counts %: {round(100*(complex_rail_network.nodes.shape[0] - merged_rail_network.nodes.shape[0])/complex_rail_network.nodes.shape[0], 0)}")
+print(f"Difference in edge counts: {complex_rail_network.edges.shape[0] - merged_rail_network.edges.shape[0]}")
+print(f"Difference in edge counts %: {round(100*(complex_rail_network.edges.shape[0] - merged_rail_network.edges.shape[0])/complex_rail_network.edges.shape[0], 0)}")
+
+merged_rail_graph = _network_to_nx(merged_rail_network)
+
+G=nx.MultiDiGraph(merged_rail_graph)
+
+for _, attr in G.nodes(data=True):
+    attr['demand'] = int(0)
+    attr['pos'] = (attr['geometry'].x, attr['geometry'].y)
+
+bbox=get_bbox(G)
+
+od_data_source = r'C:\Data\interim\od_data_2.csv'
+od_data = pd.read_csv(od_data_source)
+# Drop rows where 'geometry' is NaN
+od_data = od_data.dropna(subset=['geometry'])
+od_data['geometry'] = od_data['geometry'].apply(wkt.loads)
+bbox_shape=box(bbox[0],bbox[1], bbox[2], bbox[3])
+od_data = od_data[od_data['geometry'].apply(lambda geom: bbox_shape.contains(geom))]
+
+G = set_node_demand(od_data, G)
+G = equalize_demand_supply(G)
+G = set_edge_capacities(G)
+G = set_edge_weights(H.assets.assets, G)
+
+#Source: SA
+max_weight_graph = max(attr['weight'] for _, _, attr in G.edges(data=True))
+print('Max weight: '+str(max_weight_graph))
+max_capacity_graph = max(attr['capacity'] for _, _, attr in G.edges(data=True))
+print('Max capacity: '+str(max_capacity_graph))
+
+# create a virtual node with dummy nodes
+G_v=G.copy()
+# convert to int
+for u, v, key, attr in G.edges(keys=True, data=True):
+    G_v.add_edge((str(u) + '_d'), (str(v) + '_d'), **attr)
+
+for u in G.nodes:
+    G_v.add_edge(u,(str(u) + '_d'),capacity=max_capacity_graph*100,weight=int(round(max_weight_graph*1e3,0)))
+    G_v.add_edge((str(u) + '_d'),u,capacity=max_capacity_graph*100,weight=0)
+
+# verify capacities, weights and demands are integers
 edge_capacities_types, edge_weights_types, node_demands_types = inspect_graph(G_v)
 print("Edge Capacities Types:", set(edge_capacities_types))
 print("Edge Weights Types:", set(edge_weights_types))
 print("Node Demands Types:", set(node_demands_types))
+if {type(int())} == set(list(edge_capacities_types) + list(edge_weights_types) + list(node_demands_types)):
+    print('Success: only int type values')
+else: 
+    print('Warning! Not all values are integers')
+
+# solve the min cost flow problem
+mcf=nx.min_cost_flow(G_v)
+print('Minimum cost flow: '+str(mcf))
+
+flow_dict = mcf
+
+#visualisation
+import re
+from math import ceil
+extract_numeric_part = lambda s: int(re.search(r'\d+', s).group()) if re.search(r'\d+', s) else None
+
+for u,attr in G_v.nodes(data=True):
+    try:
+        if isinstance(u, int): 
+            pass
+        elif isinstance(u, str): 
+            if u == 'source_sink_d':
+                attr['pos']=(G.nodes['source_sink']['pos'][0],G_v.nodes['source_sink']['pos'][-1])
+            else:
+                attr['pos']=(G_v.nodes[extract_numeric_part(u)]['pos'][0]+0.0005,G_v.nodes[extract_numeric_part(u)]['pos'][-1]+0.0005)
+    except:
+        print(f'Error in setting node {u} position')    
+pos=nx.get_node_attributes(G_v, 'pos')
+nodelist=[u for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0]
+edgelist=[(u, v) for u, v, attr in G.edges(data=True) if (type(u) == int and type(v) == int)]
+node_size=[int(abs(ceil(attr['demand']/100))) for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0]
+width=[int(ceil(mcf[u][v][0]/500)+1) for u, v in edgelist]
+# draw network
+nx.draw_networkx(G_v, pos,
+        nodelist=nodelist,
+        edgelist=edgelist,
+        with_labels=False, 
+        arrows=False,
+        node_size=node_size,
+        node_color=['red' if attr['demand'] > 0 else 'blue' for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0],
+        width=width,
+        edge_color=None)
+
+
+#TODO: add background map, but needs to convert projection
+
+
+# import matplotlib.pyplot as plt
+# import cartopy.crs as ccrs
+# import networkx as nx
+
+# # Create a subplot axes
+# fig, ax = plt.subplots(subplot_kw={'projection': ccrs.Mercator()})
+
+# # Add a background image
+# ax.stock_img()
+
+# # Set the extent of the map to your area of interest
+# # Replace the coordinates with the boundaries of your area
+# ax.set_extent(bbox)
+
+# # Convert node positions to map coordinates
+# pos = {node: coordinates for node, coordinates in nx.get_node_attributes(G_v, 'pos').items()}
+
+# # Draw the network on the subplot axes
+# nx.draw_networkx(G_v, pos,
+#     nodelist=[u for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0],
+#     edgelist=[(u, v) for u, v, attr in G.edges(data=True) if (type(u) == int and type(v) == int)],
+#     with_labels=False, 
+#     arrows=False,
+#     node_size=[int(abs(round(attr['demand']/500))) for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0],
+#     node_color=['red' if attr['demand'] > 0 else 'blue' for u, attr in G_v.nodes(data=True) if 'demand' in attr and attr['demand'] != 0],
+#     width=1,
+#     edge_color=None)
+
+# # Show the plot
+# plt.show()
