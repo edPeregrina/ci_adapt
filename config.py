956e6f602ae9e683c09e4e1494b0536bf9b0168a
Daniel Peregrina Gonzalez <99656768+edPeregrina@users.noreply.github.com>
2024-05-22 10:52:32 +0200

    splitting cells for speed up

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ config['DEFAULT'] = {
     'country_code': 'DEU',
     'country_name': 'Germany',
     'hazard_data_subfolders': 'raw_subsample/validated_geometries',
-    'asset_data': 'Exposure/raw_rail_track_study_area_KOBLENZ_BONN.geojson',#Exposure/raw_rail_track_study_area_Rhine_Alpine_DEU.geojson', 
+    'asset_data': 'Exposure/raw_rail_track_study_area_Rhine_Alpine_DEU.geojson',#'Exposure/raw_rail_track_study_area_KOBLENZ_BONN.geojson',
     'vulnerability_data': 'Vulnerability'}
 
 with open('config_ci_adapt.ini', 'w') as configfile:
